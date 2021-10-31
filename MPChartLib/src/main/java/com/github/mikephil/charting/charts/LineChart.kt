ffa98d4b739c8f62f37de259015fd270c951edd3
Matt Jarjoura <jarjoura@gmail.com>
2021-10-31 10:12:04 -0700

    [kotlin] Convert LineChart to Kotlin

--- MPChartLib/src/main/java/com/github/mikephil/charting/charts/LineChart.kt
+++ MPChartLib/src/main/java/com/github/mikephil/charting/charts/LineChart.kt
@@ -1,50 +1,40 @@
+package com.github.mikephil.charting.charts
 
-package com.github.mikephil.charting.charts;
-
-import android.content.Context;
-import android.util.AttributeSet;
-
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
-import com.github.mikephil.charting.renderer.LineChartRenderer;
+import android.content.Context
+import android.util.AttributeSet
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
+import com.github.mikephil.charting.renderer.LineChartRenderer
 
 /**
  * Chart that draws lines, surfaces, circles, ...
  *
  * @author Philipp Jahoda
  */
-public class LineChart extends BarLineChartBase<LineData> implements LineDataProvider {
+class LineChart : BarLineChartBase<LineData?>, LineDataProvider {
+  constructor(context: Context?) : super(context)
 
-    public LineChart(Context context) {
-        super(context);
+  constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+  constructor(
+      context: Context?,
+      attrs: AttributeSet?,
+      defStyle: Int
+  ) : super(context, attrs, defStyle)
+
+  override fun init() {
+    super.init()
+    mRenderer = LineChartRenderer(this, mAnimator, mViewPortHandler)
+  }
+
+  override val lineData: LineData
+    get() = mData!!
+
+  override fun onDetachedFromWindow() {
+    // releases the bitmap in the renderer to avoid oom error
+    if (mRenderer != null && mRenderer is LineChartRenderer) {
+      (mRenderer as LineChartRenderer).releaseBitmap()
     }
-
-    public LineChart(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public LineChart(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-
-        mRenderer = new LineChartRenderer(this, mAnimator, mViewPortHandler);
-    }
-
-    @Override
-    public LineData getLineData() {
-        return mData;
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        // releases the bitmap in the renderer to avoid oom error
-        if (mRenderer != null && mRenderer instanceof LineChartRenderer) {
-            ((LineChartRenderer) mRenderer).releaseBitmap();
-        }
-        super.onDetachedFromWindow();
-    }
+    super.onDetachedFromWindow()
+  }
 }
